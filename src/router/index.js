09da66bb6ee40e16214e015aaabf150bf99ba6b2
RikkaPaste <3235746844@qq.com>
2022-04-20 09:19:30 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -14,19 +14,31 @@ export default new VueRouter({
     routes: [
         {
             path: '/home',
-            component: Home
+            component: Home,
+            meta:{show:true}
         },
         {
             path: '/login',
-            component: Login
+            component: Login,
+            meta:{show:false}
         },
         {
             path: '/register',
-            component: Register
+            component: Register,
+            meta:{show:false}
         },
         {
-            path: '/search',
-            component: Search
+            path: '/search/:keyword',
+            component: Search,
+            meta:{show:true},
+            name:'search',
+            //路由组件能不能传递props数据?
+            //布尔值写法
+            // props:true
+            //对象写法:额外的给路由组件传递一些props
+            // props:{a:1,b:2}
+            //函数写法：可以把params参数、query参数，通过props传递给路由组件
+            props:({params:{keyword},query:{k}})=>({keyword,k})
         },
         //重定向，在项目跑起来的时候，访问/,立马让他定向到首页
         {
